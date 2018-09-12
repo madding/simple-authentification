5db342b451c5e1b9daf2b326f284d3a02f6e4194
Eugene Gavrilov <gavrilov.ea@gmail.com>
2018-09-12 23:42:28 +0500

    Revert "fix sign up success redirect"
    This reverts commit 024b9146aec3531bfc4762ef3bd99a27463d15fb.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_session_path, notice: 'User was successfully created. Please sign in now.'
+      redirect_to @user, notice: 'User was successfully created.'
     else
       render :new, layout: 'authentification'
     end
